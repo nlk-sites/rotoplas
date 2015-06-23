ca57aa61dca2886a5164dc831ff3d705a7d812c2
AmitDeo <magpie.arise@gmail.com>
2015-06-23 12:09:14 +0545

    Homepage General Fix

--- wp-content/themes/rotoplas/header.php
+++ wp-content/themes/rotoplas/header.php
@@ -19,6 +19,7 @@
 <link href="<?php bloginfo('template_url'); ?>/css/bootstrap.min.css" rel="stylesheet">
 
 <!-- Custom CSS -->
+<link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/jquery.mobile.min.css" />
 <link href="<?php bloginfo('template_url'); ?>/css/custom.css" rel="stylesheet">
 
 <!-- Custom WebFont -->
@@ -35,35 +36,52 @@
 <?php wp_enqueue_script('jquery'); ?>
 <?php wp_head(); ?>
 <script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/bootstrap.min.js"></script>
+<script src="<?php bloginfo('template_url'); ?>/js/jquery.mobile.min.js"></script>
 </head>
-
+<script type="text/javascript">
+	jQuery(document).ready(function(){
+		jQuery( "#sidemenumobile" ).panel();
+		jQuery('#mobilemenuo').click(function(){
+			jQuery( "#sidemenumobile" ).panel("open");
+		});
+	});
+</script>
 <body <?php body_class(); ?>>
+<div data-role="panel" id="sidemenumobile" data-position="right" data-display="overlay">
+    <!-- panel content goes here -->
+</div>
  <section class="wrapper menucontainer">
       <div class="container">
         <div class="row">
-          <div class="col-xs-12 col-sm-2 col-md-2">
-            <h1 class="logo"><a href="<?php the_permalink('home');?>">Rotoplas</a></h1>
+          <div class="col-xs-6 col-sm-6 col-md-2">
+            <h1 class="logo"><a href="<?php bloginfo('url');?>">Rotoplas</a></h1>
           </div>
-          <div class="col-xs-12 col-sm-10 col-md-10">
-          <nav class="navbar navbar-default">          
-          <div class="container-fluid">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-              </button>
-            </div>
-
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-              <?php wp_nav_menu( array( 'menu_class'=>'nav navbar-nav','theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
-              </div><!-- /.navbar-collapse -->
-            </div><!-- /.container-fluid -->
-          </nav>
+          <div class="col-xs-12 col-sm-10 col-md-10 hidden-xs hidden-sm">
+	          <nav class="navbar navbar-default">          
+	          <div class="container-fluid">
+	            <!-- Brand and toggle get grouped for better mobile display -->
+	            <div class="navbar-header">
+	              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+	                <span class="sr-only">Toggle navigation</span>
+	                <span class="icon-bar"></span>
+	                <span class="icon-bar"></span>
+	                <span class="icon-bar"></span>
+	              </button>
+	            </div>
+	
+	            <!-- Collect the nav links, forms, and other content for toggling -->
+	            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+	              <?php wp_nav_menu( array( 'menu_class'=>'nav navbar-nav','theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+	              </div><!-- /.navbar-collapse -->
+	            </div><!-- /.container-fluid -->
+	          </nav>
           <span class="logo-circle"></span>
+        </div>
+        <div class="col-xs-6 col-sm-6 visible-xs-block visible-sm-block">
+        	<div class="headicoblock">
+        		<div class="logo-circle-mb"></div>
+        		<div class="menu-ico-mb"><a href="#sidemenumobile" id="mobilemenuo">MENU</a></div>
+        	</div>
         </div> 
       </div>
       </div>
--- wp-content/themes/rotoplas/template-homepage.php
+++ wp-content/themes/rotoplas/template-homepage.php
@@ -16,55 +16,74 @@ get_header(); ?>
 	<section class="wrapper content">
       <div class="container">
         <div class="row">
-            <div class="col-xs-12 col-sm-8 col-md-8">
-              <div class="row">
-              <div class="col-xs-12">
-                <ul class="servicequality">
-                  <li><a href="#">Service</a></li>
-                  <li class="bullet"><a href="#">Quality</a></li>
-                  <li class="bullet"><a href="#">Innovation</a></li>
-                </ul>
+            <div class="col-xs-12 col-sm-12 col-md-8">
+              <div class="row hidden-xs hidden-sm">
+	              <div class="col-xs-12">
+	                <ul class="servicequality">
+	                  <li><a href="#">Service</a></li>
+	                  <li class="bullet"><a href="#">Quality</a></li>
+	                  <li class="bullet"><a href="#">Innovation</a></li>
+	                </ul>
+	              </div>
               </div>
-              </div>
               <div class="row">
+              	<div class="col-xs-12">
                   <?php putRevSlider( "homeslider" ) ?>
+                </div>
               </div>
             </div>
-            <div class="col-xs-12 col-sm-4 col-md-4">
-                <div class="sidebar">
-                    <div class="col-xs-12">
-                      <h1 class="newsicon">NEWS</h1>
-                    </div>
-                  <div class="row eachcontent">
-                    <div class="col-xs-12 col-sm-5 col-md-5 imagecontent">
-                      <img class="img-responsive newsimage" src="<?php bloginfo('template_url'); ?>/images/newsimage1.jpg">
-                    </div>
-                    <div class="col-xs-12 col-sm-7 col-md-7">
-                        <p>Rotoplas conducts research and develops new technologies through our Center for Research and Development. </p>
-                        <button type="button" class="btn newsbutton">View more</button>
-                    </div>
-                  </div>
-                  <div class="row eachcontent">
-                    <div class="col-xs-12 col-sm-5 col-md-5 imagecontent">
-                        <img class="img-responsive newsimage" src="<?php bloginfo('template_url'); ?>/images/newsimage2.jpg">
-                    </div>
-                    <div class="col-xs-12 col-sm-7 col-md-7">
-                        <p>Our training programs provide a better quality of life for construction and professional workers.</p>
-                        <button type="button" class="btn newsbutton">View more</button>
-                    </div>
-                  </div>
-                  <div class="row content">
-                    <div class="col-xs-12 col-sm-5 col-md-5 imagecontent">
-                        <img class="img-responsive newsimage" src="<?php bloginfo('template_url'); ?>/images/newsimage3.jpg">
-                    </div>
-                    <div class="col-xs-12 col-sm-7 col-md-7">
-                        <p>Rotoplas creates different water solutions products, built to help those with lower incomes.</p>
-                        <button type="button" class="btn newsbutton">View more</button>
-                    </div>
-                   </div> 
+            <div class="col-xs-12 col-sm-12 col-md-4">
+                <div class="row sidebar">
+                 <div class="col-xs-12">
+	                	<h1 class="newsicon">NEWS</h1>
+	              </div>
+	              <div class="col-xs-12 col-sm-6 col-md-12 eachNews">
+						<div class="row eachcontent">
+							<div class="col-xs-12 col-sm-5 col-md-5 imagecontent">
+								<img class="img-responsive newsimage" src="<?php bloginfo('template_url'); ?>/images/newsimage1.jpg">
+							</div>
+							<div class="col-xs-12 col-sm-7 col-md-7">
+								<p>
+									Rotoplas conducts research and develops new technologies through our Center for Research and Development.
+								</p>
+								<button type="button" class="btn newsbutton">
+									View more
+								</button>
+							</div>
+						</div>
+					</div>
+					<div class="col-xs-12 col-sm-6 col-md-12 eachNews">
+						<div class="row eachcontent">
+							<div class="col-xs-12 col-sm-5 col-md-5 imagecontent">
+								<img class="img-responsive newsimage" src="<?php bloginfo('template_url'); ?>/images/newsimage2.jpg">
+							</div>
+							<div class="col-xs-12 col-sm-7 col-md-7">
+								<p>
+									Our training programs provide a better quality of life for construction and professional workers.
+								</p>
+								<button type="button" class="btn newsbutton">
+									View more
+								</button>
+							</div>
+						</div>
+					</div>
+					<div class="col-xs-12 col-sm-6 col-md-12 eachNews">
+						<div class="row eachcontent">
+							<div class="col-xs-12 col-sm-5 col-md-5 imagecontent">
+								<img class="img-responsive newsimage" src="<?php bloginfo('template_url'); ?>/images/newsimage3.jpg">
+							</div>
+							<div class="col-xs-12 col-sm-7 col-md-7">
+								<p>
+									Rotoplas creates different water solutions products, built to help those with lower incomes.
+								</p>
+								<button type="button" class="btn newsbutton">
+									View more
+								</button>
+							</div>
+						</div>
+					</div>
                 </div>
             </div>
-        
       </div>
      </div>  
     </section>
