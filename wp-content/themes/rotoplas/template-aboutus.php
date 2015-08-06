f20ce2e3a7017856b50d6cc0a4f51971404777ac
AmitDeo <magpie.arise@gmail.com>
2015-08-06 15:42:46 +0545

    About Us Content editor

--- wp-content/themes/rotoplas/template-aboutus.php
+++ wp-content/themes/rotoplas/template-aboutus.php
@@ -19,21 +19,22 @@ get_header(); ?>
             <div class="col-xs-12 col-sm-12 col-md-12">
               <div class="row">
 	              <div class="col-xs-12 aboutcontenttitle">
-	                <h1 class="user">ABOUT US</h1>
+	                <h1 class="user"><?php the_title(); ?></h1>
 	              </div>
               </div>
-                <div class="row aboutcontent">
-                  <div class="col-xs-12 col-sm-4 col-md-4 aboutusimage">
-                      <img class="img-responsive" src="<?php bloginfo('template_url'); ?>/images/map.png">
-                  </div>
-                  <div class="col-xs-12 col-sm-8 col-md-8 text">
-                      <p><span>Since 1978, our mission is to help improve the quality of life for our end  consumers.</span> Every day we focus on creating solutions to help make water more readily available, clean and clear. </p>
-                      <p><span>Rotoplas is a globally recognized leader in the designed and manufacturing of water, feed and chemical storage polyethylene solutions.</span> These heritage products and commitment to service, quality and innovation is what's driven our growth accross the United States, Mexico and the rest of Lation America; bringing to market storage, handling purification and water treatment solutions currently produced throught <span>26 plants across both continents with the newest plant in Merced, CA.<span> By leveraging our production expertise and manufacturing capacity, we commit to having product when you need it most.</p>
-                      <a href="#" class="btn contentbutton">About us</a>
-                      <a href="#" class="btn contentbutton">Mission / Vision</a>
-                      <a href="#" class="btn contentbutton">Careers</a>     
-                  </div>
-                </div>  
+               <?php while ( have_posts() ) : the_post(); ?>
+	                <div class="row aboutcontent">
+	                  <div class="col-xs-12 col-sm-4 col-md-4 aboutusimage">
+	                      <?php the_post_thumbnail('large', array('class' => 'img-responsive')); ?>
+	                  </div>
+	                  <div class="col-xs-12 col-sm-8 col-md-8 text">
+	                      <?php the_content(); ?>
+	                      <a href="#" class="btn contentbutton">About us</a>
+	                      <a href="#" class="btn contentbutton">Mission / Vision</a>
+	                      <a href="#" class="btn contentbutton">Careers</a>     
+	                  </div>
+	                </div>  
+                <?php endwhile; // End of the loop. ?>
                 <div class="row">
                   <div class="overviewcontent">
                     <h4>OVERVIEW</h4>
@@ -47,14 +48,13 @@ get_header(); ?>
 		                          </div>
 		                          <div class="col-xs-12 col-sm-3 col-md-4 overvieweachcontent">
 		                            <h4>1989</h4>
-		                            <p>A new company was founded and a new brand was created with our first product.</p>
-		                              
+		                            <p>A new company was founded and a new brand was created with our first product.</p>  
 		                          </div>
 		                          <div class="col-xs-12 col-sm-3 col-md-2 overviewimage">
 		                            <img class="img-responsive" src="<?php bloginfo('template_url'); ?>/images/overviewimage2.jpg">
 		                          </div>
 		                          <div class="col-xs-12 col-sm-3 col-md-4 overvieweachcontent">
-		                             <h4>1991</h4>
+		                              <h4>1991</h4>
 		                              <p>More plants and distribution centers were founded.</p>
 		                          </div>
 	                          </div>
