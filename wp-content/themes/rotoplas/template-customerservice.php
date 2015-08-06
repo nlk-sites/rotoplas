65a88579feb6aadd49f6c7611b327873b67ee5a0
AmitDeo <magpie.arise@gmail.com>
2015-08-06 18:09:31 +0545

    Customer Service Contact Form

--- wp-content/themes/rotoplas/template-customerservice.php
+++ wp-content/themes/rotoplas/template-customerservice.php
@@ -16,30 +16,18 @@ get_header(); ?>
 	<section class="wrapper content">
       <div class="container">
         <div class="row">
-            <div class="col-xs-12 col-sm-8 col-md-8">
+            <div class="col-xs-12 col-sm-12 col-md-12">
               <div class="row">
-              <div class="col-xs-12 contenttitle">
-                <h1 class="customerservice">COSTUMER SERVICE</h1>
-                <h2>You have any questions?</h2>
+	              <div class="col-xs-12 contenttitle">
+	                <h1 class="customerservice">COSTUMER SERVICE</h1>
+	                <h2>You have any questions?</h2>
+	              </div>
               </div>
-              </div>
-                <div class="row">
+              <div class="row">
                   <div class="col-xs-12 col-sm-5 col-md-5">
-                        <div class="contactform">
-                          <form class="contact-form">
-                            <label>NAME:</label>
-                            <input type="text" name="name" class="form-control">  
-                            <label>STATE:</label>
-                            <input type="text" name="name" class="form-control">
-                            <label>EMAIL:</label>
-                            <input type="email" name="name" class="form-control">
-                            <label>TELEPHONE:</label>
-                            <input type="text" name="name" class="form-control">
-                            <label>COMMENTS:</label>
-                            <textarea class="form-control"></textarea>
-                            <button type="button" class="btn contactformbutton">Send</button>
-                          </form> 
-                        </div>  
+                        <?php while ( have_posts() ) : the_post(); ?>
+	                        <?php the_content(); ?>
+                        <?php endwhile; // End of the loop. ?>  
                   </div>
                   <div class="col-xs-12 col-sm-7 col-md-7">
                       <img class="img-responsive maplocation" src="<?php bloginfo('template_url'); ?>/images/customermap.png">
@@ -49,55 +37,18 @@ get_header(); ?>
                         <h5>Merced, California</h5>
                       </div> 
                       <div class="row"> 
-                        <div class="col-xs-12 col-sm-6 col-md-6">
+                        <div class="col-xs-12 col-sm-12 col-md-12">
                           <div class="dealers">
-                            <h2>NAME EXAMPLE</h2>
-                            <p class="phone">(209) 723 5000</p>
-                            <p class="email">test@rotoplasusa.com</p>
+                            <h2>AMELIA ESTREDA</h2>
+                            <p class="phone">(209) 209 777 6119</p>
+                            <p class="email"><a href="mailto: aestrada@rotoplas.com">aestrada@rotoplas.com</a></p>
                           </div>
                         </div>  
-                        <div class="col-xs-12 col-sm-6 col-md-6">
-                          <div class="dealers">
-                            <h2>NAME EXAMPLE</h2>
-                            <p class="phone">(209) 723 5000</p>
-                            <p class="email">test@rotoplasusa.com</p>
-                          </div>
-                        </div> 
                       </div>
                   </div>
-                </div>    
-              </div>
-            <div class="col-xs-12 col-sm-4 col-md-4">
-                <div class="sidebar">
-                  <div class="row contacteachcontent1">
-                    <div class="col-xs-12 col-sm-5 col-md-5 imagecontent">
-                      <img class="img-responsive newsimage" src="<?php bloginfo('template_url'); ?>/images/customerserviceimage1.jpg">
-                    </div>
-                    <div class="col-xs-12 col-sm-7 col-md-7">
-                        <p>Rotoplas conducts research and develops new technologies through our Center for Research and Development. </p>
-                        <button type="button" class="btn newsbutton">View more</button>
-                    </div>
-                  </div>
-                  <div class="row eachcontent">
-                    <div class="col-xs-12 col-sm-5 col-md-5 imagecontent">
-                        <img class="img-responsive newsimage" src="<?php bloginfo('template_url'); ?>/images/customerserviceimage2.jpg">
-                    </div>
-                    <div class="col-xs-12 col-sm-7 col-md-7">
-                        <p>Our training programs provide a better quality of life for construction and professional workers.</p>
-                        <button type="button" class="btn newsbutton">View more</button>
-                    </div>
-                  </div>
-                  <div class="row content">
-                    <div class="col-xs-12 col-sm-5 col-md-5 imagecontent">
-                        <img class="img-responsive newsimage" src="<?php bloginfo('template_url'); ?>/images/customerserviceimage3.jpg">
-                    </div>
-                    <div class="col-xs-12 col-sm-7 col-md-7">
-                        <p>Rotoplas creates different water solutions products, built to help those with lower incomes.</p>
-                        <button type="button" class="btn newsbutton">View more</button>
-                    </div>
-                   </div> 
-                </div>
-            </div>
-     </div>  
-    </section>
+            	</div>    
+        	</div>
+     	</div>
+     </div>	  
+   </section>
 <?php get_footer(); ?>
