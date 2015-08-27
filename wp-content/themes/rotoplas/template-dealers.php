f563b19fc0e7d5af3286a514d10dd656262e9703
Ruby <ruby@ninthlink.com>
2015-08-27 10:46:18 -0700

    RU-44 made changes to dealers page

--- wp-content/themes/rotoplas/template-dealers.php
+++ wp-content/themes/rotoplas/template-dealers.php
@@ -12,29 +12,30 @@
  */
 
 get_header(); ?>
-	<section class="wrapper content">
+  <section class="wrapper content">
       <div class="container">
         <div class="row">
             <div class="col-xs-12 col-sm-12 col-md-12">
               <div class="row">
-	              <div class="col-xs-12 contenttitle">
-	                <h1 class="dealericon">Dealers</h1>
-	              </div>
+                <div class="col-xs-12 contenttitle">
+                  <h1 class="dealericon">Dealers</h1>
+                </div>
               </div>
               <div class="row">
                   <div class="col-xs-12 col-sm-8 col-md-8">
                     <div class="address">
-                        <h3><small>Phone</small>(209) 723 5000</h3>
-                        <h4>2651 Cooper Av.</h4>
+                        <h3><small>To find a dealer nearest you call</small>(209) 723 5000</h3>
+                        <h4>2651 Cooper Ave</h4>
                         <h5>Merced, California</h5>
                       </div> 
                     <img class="img-responsive" src="<?php bloginfo('template_url'); ?>/images/dealersmap.png" style="margin-left: auto; margin-right: auto;" /> 
                   </div>
                   <div class="col-xs-12 col-sm-4 col-md-4 dealercontent">
+                    <h3>Sales Contacts</h3>
                       <div class="row"> 
                         <div class="col-xs-12">
                           <div class="dealers">
-                            <h2>BURCE HALLINAN</h2>
+                            <h2>BURCE HALLINAN<br><span class="title">Regional Sales Manager - CA</span></h2>
                             <p class="phone">(209) 205 0500</p>
                             <p class="email"><a href="mailto: bhallinan@rotoplas.com">bhallinan@rotoplas.com</a></p>
                           </div>
@@ -43,7 +44,7 @@ get_header(); ?>
                       <div class="row">   
                         <div class="col-xs-12">
                           <div class="dealers">
-                            <h2>SEAN CLARKE</h2>
+                            <h2>SEAN CLARKE<br><span class="title">National Account Manager</span></h2>
                             <p class="phone">(949) 292 9354</p>
                             <p class="email"><a href="mailto: sclark@rotoplas.com">sclark@rotoplas.com</a></p>
                           </div>
@@ -52,8 +53,8 @@ get_header(); ?>
                   </div>
                 </div>    
              </div>
-     	</div>         
+      </div>         
      </div>  
     </section>
-	
+  
 <?php get_footer(); ?>
