daa8071828301282199ac4b139d1dbba495642f2
Ruby <ruby@ninthlink.com>
2015-08-27 14:29:06 -0700

    RU-41 commented out sidebar on homepage template

--- wp-content/themes/rotoplas/template-homepage.php
+++ wp-content/themes/rotoplas/template-homepage.php
@@ -16,7 +16,7 @@ get_header(); ?>
 	<section data-role="main" class="wrapper content">
       <div class="container">
         <div class="row">
-            <div class="col-xs-12 col-sm-12 col-md-8">
+            <div class="col-xs-12 col-sm-12 col-md-8 col-md-offset-2">
               <div class="row hidden-xs hidden-sm">
 	              <div class="col-xs-12">
 	                <ul class="servicequality">
@@ -32,7 +32,7 @@ get_header(); ?>
                 </div>
               </div>
             </div>
-            <div class="col-xs-12 col-sm-12 col-md-4">
+            <!--<div class="col-xs-12 col-sm-12 col-md-4">
                 <div class="row sidebar">
                  <div class="col-xs-12">
 	                	<h1 class="newsicon">NEWS</h1>
@@ -83,7 +83,7 @@ get_header(); ?>
 						</div>
 					</div>
                 </div>
-            </div>
+            </div>-->
       </div>
      </div>  
     </section>
