2ad08ba9b227ffa2db6d5f895e102b6104bbc082
AmitDeo <magpie.arise@gmail.com>
2015-08-28 10:09:08 +0545

    Contact and Distributor page

--- wp-content/themes/rotoplas/template-distributor.php
+++ wp-content/themes/rotoplas/template-distributor.php
@@ -31,9 +31,9 @@ get_header(); ?>
                   </div>
                   <div class="col-xs-12 col-sm-7 col-md-7">
                       <div class="contactaddress">
-                        <h1><small>CALL</small>(209) 723 5000</h1>
+                        <h2><span>CALL</span>(209) 723 5000</h2>
                         <h2>2651 Cooper Av.</h2>
-                        <h3>Merced, California</h3>
+                        <h2>Merced, California</h2>
                       </div>
                   </div>
                 </div>    
