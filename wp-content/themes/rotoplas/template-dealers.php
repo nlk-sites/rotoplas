a449464d25a650b810e79bd3bd2b6eeec36e40ac
AmitDeo <magpie.arise@gmail.com>
2016-01-19 21:36:12 +0545

    Replaced Coy and removed sean from delaers page

--- wp-content/themes/rotoplas/template-dealers.php
+++ wp-content/themes/rotoplas/template-dealers.php
@@ -41,6 +41,7 @@ get_header(); ?>
                           </div>
                         </div>
                       </div> 
+                      
                       <div class="row">   
                         <div class="col-xs-12">
                           <div class="dealers">
